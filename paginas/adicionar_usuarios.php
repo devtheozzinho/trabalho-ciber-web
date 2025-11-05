abb0500e3306d924ab309e4fe98bae8ec2103d69
theozzinho <devtheozzinho@gmail.com>
2025-11-05 19:20:59 -0300

    Criado css das telas de usuários e fornecedores

---
+++ paginas/adicionar_usuarios.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Cadastro de Usuário - PHP Puro</title>
+     <link rel="stylesheet" href="../css/users.css?v=<?php echo time(); ?>">
+    
+</head>
+    <h2 class="titulo">Cadastro de Novo Usuário</h2>
+<body>
+
+<div class="container">
+  
+    <br>
+
+    <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+                
+        <input type="text" placeholder="Nome">
+        <p>
+
+        <input type="text" placeholder="Email">
+        <p>
+
+        <input type="text" placeholder="Senha">
+        <p>
+
+        <input type="text" placeholder="CPF">
+        <p>
+ 
+        <input type="text" placeholder="sexo">
+        <p>
+         
+        <input type="text" placeholder="Nacionalidade">
+        <p>
+
+        <button type="button" onclick="alert('Sucesso!')">Cadastrar</button>
+
+    </form>
+</div>
+
+</body>
+</html>
